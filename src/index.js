740066f0161e2b71e495b67fc506202eb6262350
Alivarik <45916819+Alivarik@users.noreply.github.com>
2019-10-06 14:25:49 +0300

    edit solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 module.exports = function multiply(first, second) {
-    let itog = BigInt(+first) * BigInt(+second);
-    return itog.toString();
+    let itog = Array(first.length + second.length + 1).fill(0);
+    let arrFirst = first.split("").reverse();
+    let arrSecond = second.split("").reverse();
+    arrFirst.forEach(function(elI, index) {
+        arrSecond.forEach(function(elJ, jndex) {
+            let prom = elI * elJ;
+            itog[index + jndex] += prom;
+        });
+    });
+    for (i = 0; i < itog.length; i++) {
+        let el = itog[i] % 10;
+        let div = Math.floor(itog[i] / 10);
+        itog[i] = el;
+        if (itog[i + 1]) {
+            itog[i + 1] += div;
+        } else {
+            if (div != 0) {
+                itog[i + 1] = div;
+            }
+        }
+    }
+    return itog.reverse().join("");
 };
+console.log(multiply("329568934658432659586", "58379426534596"));
--- src/indexFor.js
+++ src/indexFor.js
@@ -1,17 +1,25 @@
 module.exports = function multiply(first, second) {
-    let itog = [];
-    if (first.lengt > second.length) {
-        let arrFirst = first.split("").reverse();
-        let arrSecond = second.split("").reverse();
-    } else {
-        let arrFirst = second.split("").reverse();
-        let arrSecond = first.split("").reverse();
-    }
-    arrFirst.forEach(elI, index => {
-        arrSecond.forEach(elJ, jndex => {
+    let itog = Array(first.length + second.length + 1).fill(0);
+    let arrFirst = first.split("").reverse();
+    let arrSecond = second.split("").reverse();
+    arrFirst.forEach(function(elI, index) {
+        arrSecond.forEach(function(elJ, jndex) {
             let prom = elI * elJ;
-            itog.push(0);
-            itog[index + jndex] +=
+            itog[index + jndex] += prom;
         });
     });
+    for (i = 0; i < itog.length; i++) {
+        let el = itog[i] % 10;
+        let div = Math.floor(itog[i] / 10);
+        itog[i] = el;
+        if (itog[i + 1]) {
+            itog[i + 1] += div;
+        } else {
+            if (div != 0) {
+                itog[i + 1] = div;
+            }
+        }
+    }
+    return itog.reverse().join("");
 };
+console.log(multiply("329568934658432659586", "58379426534596"));
